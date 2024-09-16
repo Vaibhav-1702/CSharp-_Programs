e5cc3254b9cc0bbc72db997bff9743780db5d63b
Vaibhav-1702 <vaibhav.koli30@gmail.com>
2024-09-16 15:51:12 +0530

    Add project files.

---
+++ C#_Programs/Access_Private.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace C__Programs
+//{
+//    class Student
+//    {
+//        private string name = "vaibhav"; // accessible within the same class
+
+//        private void print()
+//        {
+//            Console.WriteLine("Hello from Student Class " );
+//        }
+
+//    }
+//    public class Access_Private
+//    {
+//        static void Main(string[] args)
+//        {
+//            Student s1 = new Student();
+//           Console.WriteLine("Name = {0}", s1.name);
+
+//            s1.print();
+
+//            // This gives error at line no 24 and 26 because the private members and function cannot be accessed outside the class
+//        }
+//    }
+//}
---
+++ C#_Programs/Access_Public.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace C__Programs 
+//{ 
+//    class Student
+//{
+//    public string name = "vaibhav"; // accessible for all classes
+
+//    public void print()
+//    {
+//        Console.WriteLine("Hello from Student Class ");
+//    }
+
+//}
+//     class Access_Public
+//    {
+//        static void Main(string[] args)
+//        {
+//            Student s1 = new Student();
+//        Console.WriteLine("Name = {0}", s1.name);
+
+//        s1.print();
+//        }
+//    }
+//}
+
