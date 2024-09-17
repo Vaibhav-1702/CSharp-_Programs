a1843da566163e0fefa575ff6525dd88b4e9ce3a
Vaibhav-1702 <vaibhav.koli30@gmail.com>
2024-09-17 16:53:41 +0530

    Loops in C#

---
+++ Conditional&Loops/WhileLoop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Loops
+{
+    public class WhileLoop
+    {
+        static void Main(string[] args)
+        {
+            int i, n, j, k;
+            Console.Write("Enter a Number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            i = 0;
+            j = 1;
+            Console.Write($"{i} {j}");
+
+            k = i + j;
+            while (k <= n) //print the Fibonacci series up to that number using a while loop
+            {
+                Console.Write($" {k}");
+
+                i = j;
+                j = k;
+                k = i + j;
+            }
+        }
+    }
+}
---
+++ Conditional&Loops/doWhileLoop.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Conditional_Loops
+//{
+//    public class doWhileLoop
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int i = 1, n = 5, product;
+                
+//            do // printing table of 5 using do while loop
+//            {
+//                product = n * i;
+//                Console.WriteLine("{0} * {1} = {2}", n, i, product);
+//                i++;
+//            } while (i <= 10);
+//        }
+//    }
+//}
