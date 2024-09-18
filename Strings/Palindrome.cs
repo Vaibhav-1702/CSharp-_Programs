a01c4764c51976aa32573908e4522960f1d8cb6c
Vaibhav-1702 <vaibhav.koli30@gmail.com>
2024-09-18 17:43:21 +0530

    String Problems

---
+++ Strings/Palindrome.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Strings
+//{
+//    internal class Palindrome
+//    {
+//        static void Main(string[] args)
+//        {
+//            string str1 = "ab";
+
+//            if (palin(str1))
+//            {
+//                Console.WriteLine($"String {str1} is palindrome");
+//            }
+//            else
+//            {
+//                Console.WriteLine($"String {str1} is not palindrome");
+//            }
+//        }
+
+//        static Boolean palin(string str)
+//        {  
+//            if (str.Length == 0 || str.Length == 1)
+//            {
+//                return true;
+//            }
+
+//            char[] chars = str.ToCharArray();
+//            int start = 0;
+//            int end = str.Length - 1;
+
+//            while (start < end)
+//            {
+//                char temp = chars[start];
+//                chars[start] = chars[end];
+//                chars[end] = temp;
+//                start++;
+//                end--;
+//            }
+
+//            string str2 = new string(chars);
+
+//            if (str == str2)
+//            { 
+//                return true;
+//            }
+//            return false;
+//        }
+//    }
+//}
---
+++ Strings/ReverseStr.cs
@@ -0,0 +1,50 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Strings
+//{
+//    internal class ReverseStr
+//    {
+//        static void Main(string[] args)
+//        {
+//            string str1 = "ABCDEF";
+            
+//           // Console.WriteLine(str2);
+//            Console.WriteLine(Reverse2(str1));
+
+
+//        }
+
+//        static string Reverse1(string str)
+//        {
+//            string str2 = "";
+//            for (int i = 0; i < str.Length; i++)
+//            {
+//                str2 = str[i] + str2;
+//            }
+//            return str2;
+//        }
+
+//        static string Reverse2(string str)
+//        {   
+//            char[] chars = str.ToCharArray();
+//            int start = 0;
+//            int end = str.Length-1;
+//            while (start < end)
+//            {
+//                char temp = chars[start];   
+//                chars[start] = chars[end];
+//                chars[end] = temp;
+//                start++;
+//                end--;
+//            }
+//            string str2= new string(chars);
+
+//            return str2;
+//        }
+
+//    }
+//}
