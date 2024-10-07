a783b69532576ad9bee3dceac942f271169f7788
Vaibhav-1702 <vaibhav.koli30@gmail.com>
2024-10-07 17:30:47 +0530

    Delegates

---
+++ Delegates/MultiCast.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void RectDelegeate(int width, int height);
+    public class MultiCast
+    {
+        public void RectArea(int width, int height)
+        {
+            Console.WriteLine("Area of Rectangle is : " + (width*height));
+        }
+
+        public void RectPeri(int width, int height)
+        {
+            Console.WriteLine("Perimeter of Rectangle is : " + 2*(width + height));
+        }
+        static void Main(string[] args)
+        {
+            MultiCast m1 = new MultiCast();
+            //RectDelegeate rect = new RectDelegeate(multiCast.RectArea);
+            //rect(10, 20);
+            RectDelegeate rect = m1.RectArea;
+            rect += m1.RectPeri;
+            rect(10,20);
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.Serialization;
+
+//namespace Delegates
+//{
+//    public delegate void AddDelegate(int a, int b);
+//    class Program
+//    {
+//        public void AddNum(int num1, int num2)
+//        {
+//            Console.WriteLine(num1+num2);
+//        }
+//        static void Main(string[] args)
+//        {
+//            Program p = new Program();
+//            AddDelegate ad = new AddDelegate(p.AddNum);
+//            ad.Invoke(10, 20);
+//            ad(10, 40);
+             
+            
+//        }
+//    }
+//}
