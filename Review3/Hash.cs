71c8d1eea342029bcdc0360689071ab7ffe39372
Vaibhav-1702 <vaibhav.koli30@gmail.com>
2024-10-07 13:20:46 +0530

    Review3

---
+++ Review3/Hash.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review3
+{
+    public class Hash
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+            names.Add("Vaibhav");
+            names.Add("Sujal");
+            names.Add("Raj");
+
+            Console.WriteLine("Set1");
+            foreach (string name in names) 
+            {
+                Console.WriteLine(name);
+            }
+
+
+            HashSet<string> names1= new HashSet<string>();
+
+            names1.Add("Vaibhav");
+            names1.Add("Sujal");
+            names1.Add("Raj");
+            names1.Add("Aditya");
+            names1.Add("Aniket");
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Set2");
+            foreach (string name in names1)
+            {
+                Console.WriteLine(name);
+            }
+
+            //Console.WriteLine("");
+
+            //names.UnionWith(names1);
+            //Console.WriteLine("Union");
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+
+            //Console.WriteLine("");
+
+            //names.IntersectWith(names1);
+            //Console.WriteLine("Intersect");
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+
+            Console.WriteLine("");
+
+            names1.ExceptWith(names);
+            Console.WriteLine("Expect");
+            foreach (string name in names1)
+            {
+                Console.WriteLine(name);
+            }
+
+
+
+        }
+    }
+}
---
+++ Review3/Program.cs
@@ -0,0 +1,68 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Transactions;
+
+//class Employee
+//{
+//    public string Name { get; set; }
+//    public decimal Salary { get; set; }
+//    public string Department { get; set; }
+
+//    public Employee(string name, decimal salary, string department)
+//    {
+//        Name = name;
+//        Salary = salary;
+//        Department = department;
+//    }
+//}
+
+//class Program
+//{
+    
+//    public static List<Employee> SearchByDepartment(List<Employee> employees, string department)
+//    {
+//       // return employees.Where(emp => emp.Department == department).ToList();
+
+//            List<Employee> result = new List<Employee>();  
+
+//            foreach (Employee emp in employees)
+//            {
+//                if (emp.Department == department)  
+//                {
+//                    result.Add(emp); 
+//                }
+//            }
+
+//            return result;  
+//        }
+    
+
+//    static void Main(string[] args)
+//    {
+        
+//        List<Employee> employees = new List<Employee>
+//        {
+//            new Employee("Vaibhav", 50000, "IT"),
+//            new Employee("Sujal", 60000, "HR"),
+//            new Employee("Raj", 55000, "IT"),
+//            new Employee("Rohit", 52000, "Finance")
+//        };
+
+
+//        Console.WriteLine("Departments");
+//        Console.WriteLine("HR , IT , Finance");
+//        Console.Write("Enter the name of the Deparment From Above :");
+//        string depart = Console.ReadLine();
+
+
+       
+//        List<Employee> itEmployees = SearchByDepartment(employees, depart);
+
+       
+//        foreach (Employee emp in itEmployees)
+//        {
+//            Console.WriteLine($"Name: {emp.Name}, Salary: {emp.Salary}, Department: {emp.Department}");
+//        }
+//    }
+//}
